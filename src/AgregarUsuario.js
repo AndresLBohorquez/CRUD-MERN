5239f80d0c12bd2b1ed08ca1395d3de3cb813bee
AndresLBohorquez <andresleonardo.6@gmail.com>
2022-11-23 13:10:51 -0500

    1

---
+++ src/AgregarUsuario.js
@@ -0,0 +1,10 @@
+import React from 'react';
+function AgregarUsuario(){
+    return(
+        <div>
+            <h2>Agregar nuevo usuario</h2>
+        </div>
+    )
+}
+
+export default AgregarUsuario;
--- src/App.js
+++ src/App.js
@@ -1,23 +1,20 @@
-import logo from './logo.svg';
-import './App.css';
+import AgregarUsuario from "./AgregarUsuario";
+import "./App.css";
+import EditarUsuario from "./EditarUsuario";
+import ListaUsuarios from "./ListaUsuarios";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h1>CRUD MERN STACK</h1>
+      <BrowserRouter>
+        <Routes>
+          <Route path="/" element={<ListaUsuarios />}></Route>
+          <Route path="/agregarusuario" element={<AgregarUsuario />}></Route>
+          <Route path="/editarusuario" element={<EditarUsuario />}></Route>
+        </Routes>
+      </BrowserRouter>
     </div>
   );
 }
